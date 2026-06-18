d953788fe6e4a58535efb33a02253c0b57845a6b
Jeffinroy <jeffinroy01@gmail.com>
2026-06-18 11:24:30 +0530

    Delete ut directory

--- ut/supporting-objects/deinstall-script.sql
+++
@@ -1,4 +0,0 @@
-drop table EBA_UT_chart_projects       cascade constraints;
-drop table EBA_UT_chart_tasks          cascade constraints;
-drop table EBA_UT_demo_cards           cascade constraints;
-drop table EBA_UT_map_airports         cascade constraints;
